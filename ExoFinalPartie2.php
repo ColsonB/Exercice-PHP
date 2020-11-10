15ff2aa5b9c9a2ead629b9f26b43eded200bb0b2
Baptiste Colson <bcolson@la-providence.net>
2020-11-10 14:48:44 +0100

    Commit

--- ExoFinalPartie2.php
+++ ExoFinalPartie2.php
@@ -1,14 +1,30 @@
 <?php
-Function Formulaire () {
 
- echo <
+Function Formulaire ($motdepasse) {
+
+   echo "<form>
+    <div class="form-example">
+      <label for="name">Identifiant</label>
+      <input type="text" name="name" id="name" required>
+    </div>
+    <div class="form-example">
+      <label for="name">Mot de passe</label>
+      <input type="name" name="name" id="name" required>
+    </div>
+    <div class="form-example">
+      <input type="submit" value="Valider">
+    </div>
+  </form>";
+
+  highlight_file(__FILE__);
 
 }
+
 ?>
 
 <html>
 
-<head><title>Exercice Final</title></head>
+<head><title>Exercice Final Partie 2</title></head>
 
 <body>
 
