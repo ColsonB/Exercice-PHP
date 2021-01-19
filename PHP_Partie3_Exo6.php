31c2a1b32b6baf6d814b962621866289136c8c53
Baptiste Colson <bcolson@la-providence.net>
2021-01-19 16:03:42 +0100

    Commit

---
+++ PHP_Partie3_Exo6.php
@@ -0,0 +1,38 @@
+<?php
+include "personnage.php";
+try{
+    $pdo= new PDO ('mysql:host=192.168.64.106;dbname=Exo5_Objet_Colson', 'colson', 'colson');
+    $p1 = new Personnage(1, $pdo);
+    $p2 = new Personnage(2, $pdo);
+    $p3 = new Personnage(3, $pdo);
+}
+catch(Exception $e){
+    die('Erreur : ' . $e->getMessage());
+} 
+?>
+
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exercice 4</title>
+    </head>
+    <body>
+        <?php
+            $p1->affiche();
+            $p2->affiche();
+            $p3->affiche();
+
+            $p2->attaque($p1);
+            $p1->défense($p2);
+            $p1->attaque($p3);
+            $p3->attaque($p2);
+            
+            $p1->affichePV();
+            $p2->affichePV();
+            $p3->affichePV();
+
+            $p2->Soin();
+        ?>
+    </body>
+</html>
--- personnage.php
+++ personnage.php
@@ -7,11 +7,10 @@
         private $_ID;
         private $_User;
         
-
-        //Méthode on attend id du perso et l'objet pdo instancé
+        //Méthode on attend id du personnage et l'objet pdo instancé
         public function __construct($ID, $pdo)
         {
-            $this->_ID= $ID;
+            $this->_ID = $ID;
             $this->_BDD = $pdo;
             $req = "SELECT * FROM personnages WHERE ID = $this->_ID";            
             $this->_User= $this->_BDD->query($req)->fetch();
@@ -23,30 +22,34 @@
 
         public function affiche()
         {
-            echo "<p>Votre vie est de ".$this->_User['Force']." et votre pseudo est ".$this->_User['Pseudo'];
-            
+            echo "<p>Votre vie est de ".$this->_User['Force']." et votre pseudo est ".$this->_User['Pseudo']; 
         }
+
         public function getPseudo()
         {
-            return  $this->_User['Pseudo'];
-            
+            return  $this->_User['Pseudo']; 
         }
+
         public function attaque($PersoAttaquant)
         {
             echo "<p>".$PersoAttaquant->getPseudo()." vous attaque et vous perdez ".$PersoAttaquant->getAttaque()." point de vie ";
             $this->_User['Vie']=$this->_User['Vie']-$PersoAttaquant->getAttaque();
         }
+
         public function défense($PersoDéfend)
         {
-            echo "<p>".$PersoDéfend->getPseudo().", contre l'attaque avec succés ";
-            $this->_User['Vie']=$this->_User['Vie']+10;
+            $req1 = "UPDATE personnages SET Vie = $this->_ID WHERE ID = $this->_ID";
         }
+
         public function affichePV()
         {
             echo "<p>".$this->_User['Pseudo'].", il vous reste ".$this->_User['Vie']." point de vie ";
         }
-        
 
+        public function Soin()
+        {
+            $req3 = "UPDATE personnages SET Vie = 100 WHERE ID = $this->_ID";
+        }
     }
 
     highlight_file(__FILE__);
